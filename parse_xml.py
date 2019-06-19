79120b76056623f9a96a76f96125accf8188a8f0
Fabian C. Moss <fabianmoss@gmail.com>
2019-06-19 14:18:51 +0200

    update parser

--- parse_xml.py
+++ parse_xml.py
@@ -4,6 +4,6 @@ import sys
 
 if __name__ == "__main__":
     infile = sys.argv[1]
-    name, ext = infile.split(".")
+    name, ext = infile.rsplit(".", 1)
     df = xml_to_csv(infile)
     df.to_csv(name+".csv")
