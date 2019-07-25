1295221a534ce6d49eb7c5008f32c6e54d96645f
Robert Lieck (SilverBullet) <robert.lieck@gmail.com>
2019-07-25 17:10:15 +0200

    updating tests
    old and new implementation of diffusion method should produce equivalent results (up to 4 decimals)

--- test_diffusion.py
+++ test_diffusion.py
@@ -5,14 +5,28 @@ import numpy as np
 
 class TestDiffusion(TestCase):
     def test_diffuse(self):
+        # check boundary flag
         for open_boundary in [True, False]:
+            # check for small and large tonal ranges
             for min_tone, max_tone in [('F', 'G'), ('Fbb', 'B##')]:
+                # check multiple times randomized
                 for _ in range(10):
                     lof = Tone.get_lof(min_tone, max_tone)
                     tones = [Tone((idx, 0), name) for idx, name in enumerate(lof)]
-                    Tone.diffuse(tones=tones,
-                                 center="C",
-                                 atol=1e-2,
-                                 action_probs=np.random.uniform(0, 1, 6),
-                                 lam=np.random.uniform(0, 1, 1),
-                                 open_boundary=open_boundary)
+                    # check against old version
+                    action_probs = np.random.uniform(0, 1, 6)
+                    lam = np.random.uniform(0, 1, 1)[0]
+                    weights_ = Tone.diffuse_(tones=tones,
+                                             center="C",
+                                             atol=1e-10,
+                                             action_probs=action_probs,
+                                             lam=lam,
+                                             open_boundary=open_boundary,
+                                             norm_lambda=True)
+                    weights = Tone.diffuse(tones=tones,
+                                           center="C",
+                                           atol=1e-10,
+                                           action_probs=action_probs,
+                                           lam=lam,
+                                           open_boundary=open_boundary)
+                    np.testing.assert_array_almost_equal(weights, weights_, decimal=4)
