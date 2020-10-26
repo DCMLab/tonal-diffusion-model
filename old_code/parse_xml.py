be3111d78dd8ad14f92fd04549536a061069099a
Robert Lieck <science@robert-lieck.com>
2020-10-26 13:21:03 +0100

    clean version of code used for TISMIR publication

--- old_code/parse_xml.py
+++
@@ -1,9 +0,0 @@
-from pitchplots.parser import xml_to_csv
-import pandas as pd
-import sys
-
-if __name__ == "__main__":
-    for f in sys.argv[1:]:
-        name, ext = f.rsplit(".", 1)
-        df = xml_to_csv(f)
-        df.to_csv(name+".csv")
